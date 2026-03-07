947d40fdd37ffcb2d2c0e675f4f335f8ae1a2a87
shawky <mastershawky2017@gmail.com>
2026-03-07 21:07:13 +0200

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>HomePage
+      <SignedOut>
+        <SignOutButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
   )
-}
+};
 
-export default HomePage
+export default HomePage;
