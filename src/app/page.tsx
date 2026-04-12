b7505ef6c5f792b6fe64ff756d7527d5d6891bc1
shawky <mastershawky2017@gmail.com>
2026-04-12 19:37:09 +0200

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,127 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
+import TerminalOverlay from "@/components/TerminalOverlay";
+import { Button } from "@/components/ui/button";
+import UserPrograms from "@/components/UserPrograms";
+import { ArrowRightIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>HomePage
-      <SignedOut>
-        <SignOutButton />
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
+    <div className="flex flex-col text-foreground overflow-hidden min-h-screen">
+      <section className="grow relative py-24 z-10">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 gap-8 items-center relative lg:grid-cols-12">
+            {/* CORNER DECARATION */}
+            <div className="absolute -top-10 left-0 w-40 h-40 border-l-2 border-t-2" />
+
+            {/* LEFT SIDE */}
+            <div className="relative space-y-8 lg:col-span-7">
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight">
+                <div>
+                  <span className="text-foreground">Transform</span>
+                </div>
+                <div>
+                  <span className="text-primary">Your Body</span>
+                </div>
+                <div className="pt-2">
+                  <span className="text-foreground">With Advanced</span>
+                </div>
+                <div className="pt-2">
+                  <span className="text-foreground">AI</span>
+                  <span className="text-primary"> Technology</span>
+                </div>
+              </h1>
+
+              {/* SEPERATOR LINE */}
+              <div className="h-px w-full bg-linear-to-r from-primary via-secondary to-primary opacity-50"></div>
+
+              <p className="text-xl text-muted-foreground w-2/3">
+                Talk to our AI assistant and get personalized diet plans and workout routines
+                designed just for you
+              </p>
+
+              {/* STATS */}
+              <div className="flex items-center gap-10 py-6 font-mono">
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">500+</div>
+                  <div className="text-xs uppercase tracking-wider">ACTIVE USERS</div>
+                </div>
+                <div className="h-12 w-px bg-linear-to-b from-transparent via-border to-transparent"></div>
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">3min</div>
+                  <div className="text-xs uppercase tracking-wider">GENERATION</div>
+                </div>
+                <div className="h-12 w-px bg-linear-to-b from-transparent via-border to-transparent"></div>
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">100%</div>
+                  <div className="text-xs uppercase tracking-wider">PERSONALIZED</div>
+                </div>
+              </div>
+
+              {/* BUTTON */}
+              <div className="flex flex-col sm:flex-row gap-4 pt-6">
+                <Button
+                  size="lg"
+                  asChild
+                  className="overflow-hidden bg-primary text-primary-foreground px-8 py-6 text-lg font-medium"
+                >
+                  <Link href={"/generate-program"} className="flex items-center font-mono">
+                    Build Your Program
+                    <ArrowRightIcon className="ml-2 size-5" />
+                  </Link>
+                </Button>
+              </div>
+            </div>
+
+            {/* RIGHT SIDE */}
+             <div className="lg:col-span-5 relative">
+              {/* CORNER PIECES */}
+              <div className="absolute -inset-4 pointer-events-none">
+                <div className="absolute top-0 left-0 w-16 h-16 border-l-2 border-t-2 border-border" />
+                <div className="absolute top-0 right-0 w-16 h-16 border-r-2 border-t-2 border-border" />
+                <div className="absolute bottom-0 left-0 w-16 h-16 border-l-2 border-b-2 border-border" />
+                <div className="absolute bottom-0 right-0 w-16 h-16 border-r-2 border-b-2 border-border" />
+              </div>
+
+              {/* IMAGE CONTAINER */}
+              <div className="relative max-w-lg mx-auto">
+                <div className="relative overflow-hidden rounded-lg bg-cyber-black">
+                  <Image
+                    src="/hero-ai3.png"
+                    alt="AI Fitness Coach"
+                    width={500}
+                    height={500}
+                    className="object-cover object-center"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    priority
+                  />
+
+                  {/* SCAN LINE */}
+                  <div className="absolute inset-0 bg-[linear-gradient(transparent_0%,transparent_calc(50%-1px),var(--cyber-glow-primary)_50%,transparent_calc(50%+1px),transparent_100%)] bg-size-[100%_8px] animate-scanline pointer-events-none" />
+
+                  {/* DECORATIONS ON TOP THE IMAGE */}
+                  <div className="absolute inset-0 pointer-events-none">
+                    <div className="absolute top-1/3 left-1/3 w-1/3 h-1/3 border border-primary/40 rounded-full" />
+
+                    {/* TARGETING LINES */}
+                    <div className="absolute top-1/2 left-0 w-1/4 h-px bg-primary/50" />
+                    <div className="absolute top-1/2 right-0 w-1/4 h-px bg-primary/50" />
+                    <div className="absolute top-0 left-1/2 h-1/4 w-px bg-primary/50" />
+                    <div className="absolute bottom-0 left-1/2 h-1/4 w-px bg-primary/50" />
+                  </div>
+
+                  <div className="absolute inset-0 bg-linear-to-t from-background via-background/40 to-transparent" />
+                </div>
+
+                {/* TERMINAL OVERLAY */}
+                <TerminalOverlay />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <UserPrograms />
     </div>
   )
 };
